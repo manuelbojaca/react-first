9ef707bb89e50958c41681e084392420c261f573
Manuel Bojaca <manuel.bojaca@outlook.com>
2022-05-10 10:23:33 -0500

    fix

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Title from "./components/Title";
+import Subtitle from "./components/Subtitle";
+import Button from "./components/Button";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Title name="Manuel Bojaca" />
+        <section>
+          <Subtitle edad="30" />
+          <Button info="Click me!" />
+        </section>
       </header>
     </div>
   );
